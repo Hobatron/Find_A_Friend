ba61fcff5b6b87c3f624c537085435cbc2f4b6e4
mason ashlock <masonashlock@gmail.com>
2019-03-02 02:31:05 -0600

    carousel

--- server.js
+++ server.js
@@ -4,25 +4,30 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
+// Add our JS
+app.use('/assets', express.static(__dirname + "/app/public/assets/css"));
+app.use('/assets', express.static(__dirname + "/app/public/assets/js"));
+app.use('/assets', express.static(__dirname + "/app/public/assets/imgs"));
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-    //res.sendFile(path.join(__dirname, "view.html"));
-    console.log('success')
-    res.send('Success on port: ' + PORT);
+  res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+
+app.get("/add", function (req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/add.html"));
 });
 
 
 
 
-
-
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
