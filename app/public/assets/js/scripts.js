ac84a39c8e9ff0179105287fdb122c4655bd18f2
mason ashlock <masonashlock@gmail.com>
2019-03-04 15:48:08 -0600

    finished survey

--- app/public/assets/js/scripts.js
+++ app/public/assets/js/scripts.js
@@ -1,1 +1,19 @@
 console.log("Test")
+$.get('assets/survey.json', data => {
+    loadModel(data);
+})
+Survey.Survey.cssType = "bootstrap";
+console.log(Survey)
+
+function sendDataToServer(survey) {
+    //send Ajax request to your web server.
+    alert("The results are:" + JSON.stringify(survey.data));
+}
+
+function loadModel(surveyJSON) {
+    var survey = new Survey.Model(surveyJSON);
+    $("#surveyContainer").Survey({
+        model: survey,
+        onComplete: sendDataToServer
+    });
+}
